eb8573141602de53bc2cf046086c1dc9e44d7d2f
sahyudi <sahyudi.dev@gmail.com>
2020-03-20 08:46:33 +0700

    memperbaiki erp login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -23,7 +23,7 @@
 <body class="hold-transition login-page">
     <div class="login-box">
         <div class="login-logo">
-            <a href="<?= base_url('auth') ?>"><b>Login</b></a>
+            <a href="<?= base_url('auth') ?>"><b>Login ERP</b></a>
         </div>
         <!-- /.login-logo -->
         <div class="card">
